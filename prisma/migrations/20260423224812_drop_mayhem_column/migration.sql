3a48079f51f3f485555013d51895276c930ad9be
sollabs-app <sollabs.app@gmail.com>
2026-04-24 01:48:34 +0300

    drop mayhem column migration

---
+++ prisma/migrations/20260423224812_drop_mayhem_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isMayhemMode` on the `Token` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Token" DROP COLUMN "isMayhemMode";
